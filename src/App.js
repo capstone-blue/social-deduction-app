328aca465fb25586f404be010d87ee8433e7f68b
Ian Logan <itlogan9@gmail.com>
2020-05-07 15:11:52 -0500

    Connect app to firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { auth, db } from './firebase';
 
 function App() {
+  useEffect(() => {
+    function checkAuthStatus() {
+      auth().onAuthStateChanged(async (user) => {
+        try {
+          if (user) {
+            const uid = user.uid;
+            const snapshot = await db.ref(`/users/${uid}`).once('value');
+            if (snapshot.exists())
+              return console.log(`User ${uid} already in database!`);
+          } else {
+            console.log('entered');
+            await auth().signInAnonymously();
+            const uid = auth().currentUser.uid;
+            await db.ref(`/users/${uid}`).set({ signedIn: true });
+            console.log(`Added user ${uid} to database!`);
+          }
+        } catch (e) {
+          console.error(e);
+        }
+      });
+    }
+    checkAuthStatus();
+  }, []);
+
   return <h1>This is the App</h1>;
 }
 
