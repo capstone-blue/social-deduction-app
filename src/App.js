134876951475b95834421e7ee76f7800817254ae
Ian Logan <itlogan9@gmail.com>
2020-05-08 16:23:02 -0500

    Connect react router to display home page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { useEffect } from 'react';
 import { auth, db } from './firebase';
+import { useUser } from './context/userContext';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
 
 function App() {
+  const [, setCurrUser] = useUser();
+
   useEffect(() => {
     function checkAuthStatus() {
       auth().onAuthStateChanged(async (user) => {
         try {
           if (user) {
             const uid = user.uid;
-            const snapshot = await db.ref(`/users/${uid}`).once('value');
-            if (snapshot.exists())
-              return console.log(`User ${uid} already in database!`);
+            const userSnap = await db.ref(`/users/${uid}`).once('value');
+            if (userSnap.exists()) setCurrUser(userSnap);
           } else {
             await auth().signInAnonymously();
             const uid = auth().currentUser.uid;
@@ -23,9 +28,23 @@ function App() {
       });
     }
     checkAuthStatus();
-  }, []);
+  }, [setCurrUser]);
 
-  return <h1>This is the App</h1>;
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
+}
+
+function Routes() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={HomePage} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
