70229e68f1cc2db3e70deac817a39aba552380c2
Ian Logan <itlogan9@gmail.com>
2020-05-09 20:33:06 -0500

    Export only a user id from user context

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,18 +1,18 @@
-import React, { createContext, useContext, useState, useMemo } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
 const UserContext = createContext();
 
 function UserProvider(props) {
-  const [user, setUser] = useState({});
-  const value = useMemo(() => [user, setUser], [user]);
+  const [userId, setUserId] = useState(null);
+  const value = [userId, setUserId];
   return <UserContext.Provider value={value} {...props} />;
 }
 
-function useUser() {
+function useUserId() {
   const context = useContext(UserContext);
   if (!context) throw new Error('useUser must be used within a UserProvider');
 
   return context;
 }
 
-export { UserProvider, useUser };
+export { UserProvider, useUserId };
