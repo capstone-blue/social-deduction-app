3cbd4d36183aa22ab1171985da6e006ca2aad243
Jason Li <iverefiner@gmail.com>
2020-05-12 10:56:10 -0500

    moved GameStart component over to its own file instead of inside LobbyPage and added it to the components index

--- src/components/LobbyPage.js
+++ src/components/LobbyPage.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { db } from '../firebase';
 import { useObjectVal } from 'react-firebase-hooks/database';
 import { useUserId } from '../context/userContext';
-import { BrowserRouter as Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+import {GameStart} from './index'
 
 // Main logic should be handled in this component
 function LobbyPage({ match, history }) {
@@ -86,66 +87,6 @@ function AliasModal({ match }) {
       </form>
     </div>
   );
-}
-
-
-function GameStart({ match, players, history, }) {
-  const [userId] = useUserId();
-  const [lobbiesRef] = useState(db.ref(`/lobbies/${match.params.id}`));
-  // const [currentUser] = useState(lobbiesRef.child(`${userId}`))
-  const [minPlayers] = useState(2)
-  const [isHost, setIsHost] = useState(false)
-  const [started, setStarted] = useState('pending')
-
-
-
-  useEffect(() => {
-    async function listenOnLobby() {
-      try {
-        lobbiesRef.child('status').once('value').then(function (snapshot) {
-          setStarted(snapshot.val())
-        })
-      } catch (e) {
-        console.error('Error in GameStart lobby listener', e.message)
-      }
-    }
-    function checkIfHost() {
-      const currentPlayer = players.find(player => player[0] === userId)
-      if (currentPlayer[1].host) {
-        setIsHost(true)
-      }
-    }
-    listenOnLobby()
-    checkIfHost()
-  }, [lobbiesRef, players, userId])
-
-  async function createGameSession() {
-    // checks for min players to start game
-    if (players.length >= minPlayers) {
-      try {
-        // creates a game session by transferring lobby members data over
-        players.forEach(player => {
-          const [playerId, playerProps] = player;
-          db.ref(`/gameSessions/${match.params.id}/players`).child(`${playerId}`).set(playerProps)
-        })
-        // set lobby status from pending to started so component will render redirect to game session
-        lobbiesRef.update({ 'status': 'started' });
-      } catch (e) {
-        console.error('Error in createGameSession', e.message)
-      }
-      // lobbiesRef.set(null)
-      // history.push(`/gamesession/${match.params.id}`);
-    } else {
-      alert(`${minPlayers - players.length} more players required to start a game`)
-    }
-  }
-
-  return (
-    <div>
-      {isHost ? <button onClick={createGameSession}>Start Game</button> : <p>Waiting for host...</p>}
-      {started === 'started' ? history.push(`/gamesession/${match.params.id}`) : null}
-    </div>
-  )
 }
 
 export default LobbyPage;
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {default as HomePage} from './HomePage'
 export {default as LobbyPage} from './LobbyPage'
 export {default as GameSession} from './GameSession'
+export {default as GameStart} from './GameStart'
