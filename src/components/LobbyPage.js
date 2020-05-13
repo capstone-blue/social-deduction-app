b12b4f22d2e4df4a508617f20f88ff8142479705
Jason Li <iverefiner@gmail.com>
2020-05-12 21:33:11 -0500

    put AliasModal into its own file inside components and added to index.js

--- src/components/LobbyPage.js
+++ src/components/LobbyPage.js
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { db } from '../firebase';
 import { useObjectVal } from 'react-firebase-hooks/database';
-import { useUserId } from '../context/userContext';
-import { GameStart } from './index'
-import Modal from 'react-bootstrap/Modal'
-import Button from 'react-bootstrap/Button'
+import { GameStart, AliasModal } from './index'
 
 // Main logic should be handled in this component
 function LobbyPage({ match, history }) {
@@ -59,57 +56,6 @@ function LobbyView({ name, players }) {
         </div>
       </div>
     </div>
-  );
-}
-
-
-function AliasModal({ match }) {
-  const [show, setShow] = useState(false);
-  const [userId] = useUserId();
-  const [alias, setAlias] = useState('');
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    const lobbyId = match.params.id;
-    const updates = {
-      [`users/${userId}/alias`]: alias,
-      [`lobbies/${lobbyId}/players/${userId}/alias`]: alias,
-    };
-    db.ref().update(updates);
-  }
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  return (
-    <>
-      <Button variant="primary" onClick={handleShow}>
-        Create Alias
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>What would you like to set your name as?</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              value={alias}
-              onChange={(e) => setAlias(e.target.value)}
-            />
-            <Button type="submit" variant="primary" onClick={handleClose}>
-              Save Changes
-          </Button>
-          </form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
   );
 }
 
