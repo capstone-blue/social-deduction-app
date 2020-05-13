bce49eed0b6d44deeff8c7047b65f706c907cb97
Jason Li <iverefiner@gmail.com>
2020-05-12 21:43:59 -0500

    parted LobbyView off to its own file. added to index.js

--- src/components/LobbyPage.js
+++ src/components/LobbyPage.js
@@ -1,27 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { db } from '../firebase';
 import { useObjectVal } from 'react-firebase-hooks/database';
-import { GameStart, AliasModal } from './index'
+import { GameStart, AliasModal, LobbyView } from './index'
 import Container from 'react-bootstrap/Container'
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  font-size: 1.5em;
-  ${'' /* text-align: center; */}
-  color: darkslateblue;
-`;
-
-const SubTitle = styled.h2`
-  font-size: 1.5em;
-  ${'' /* text-align: center; */}
-  color: darkslateblue;
-`;
-
-const Text = styled.h3`
-  font-size: 1.5em;
-  ${'' /* text-align: center; */}
-  color: darkslateblue;
-`;
 
 // Main logic should be handled in this component
 function LobbyPage({ match, history }) {
@@ -31,7 +12,7 @@ function LobbyPage({ match, history }) {
 
 
   return lobbyLoading ? (
-    <div>...Loading</div>
+    <Container>...Loading</Container>
   ) : (
       <Container>
         <AliasModal match={match} />
@@ -39,46 +20,6 @@ function LobbyPage({ match, history }) {
         <GameStart players={Object.entries(lobby.players)} match={match} history={history} setGameStarted={setGameStarted} gameStarted={gameStarted} />
       </Container>
     );
-}
-
-function LobbyView({ name, players }) {
-  const [host, setHost] = useState({});
-  const [members, setMembers] = useState([]);
-  useEffect(() => {
-    const m = [];
-    let h = {};
-
-    players.forEach((playerEntry) => {
-      const [, player] = playerEntry;
-      if (player.host) h = player;
-      else m.push(playerEntry);
-    });
-
-    setHost(h);
-    setMembers(m);
-  }, [players]);
-
-  return (
-    <React.Fragment>
-      <Container>
-        <Title>Lobby</Title>
-        <SubTitle>{name}</SubTitle>
-        <Container>
-          <Text>host</Text>
-          <Container>{host.alias ? host.alias : '(...)'}</Container>
-        </Container>
-        <Container>
-          <Text>members</Text>
-          <Container>
-            {members.map((m) => {
-              const [id, player] = m;
-              return <Container key={id}>{player.alias ? player.alias : '(...)'}</Container>;
-            })}
-          </Container>
-        </Container>
-      </Container>
-    </React.Fragment>
-  );
 }
 
 export default LobbyPage;
---
+++ src/components/LobbyView.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import Container from 'react-bootstrap/Container'
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  ${'' /* text-align: center; */}
+  color: darkslateblue;
+`;
+
+const SubTitle = styled.h2`
+  font-size: 1.5em;
+  ${'' /* text-align: center; */}
+  color: darkslateblue;
+`;
+
+const Text = styled.h3`
+  font-size: 1.5em;
+  ${'' /* text-align: center; */}
+  color: darkslateblue;
+`;
+
+function LobbyView({ name, players }) {
+  const [host, setHost] = useState({});
+  const [members, setMembers] = useState([]);
+  useEffect(() => {
+    const m = [];
+    let h = {};
+
+    players.forEach((playerEntry) => {
+      const [, player] = playerEntry;
+      if (player.host) h = player;
+      else m.push(playerEntry);
+    });
+
+    setHost(h);
+    setMembers(m);
+  }, [players]);
+
+  return (
+    <React.Fragment>
+      <Container>
+        <Title>Lobby</Title>
+        <SubTitle>{name}</SubTitle>
+        <Container>
+          <Text>host</Text>
+          <Container>{host.alias ? host.alias : '(...)'}</Container>
+        </Container>
+        <Container>
+          <Text>members</Text>
+          <Container>
+            {members.map((m) => {
+              const [id, player] = m;
+              return <Container key={id}>{player.alias ? player.alias : '(...)'}</Container>;
+            })}
+          </Container>
+        </Container>
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default LobbyView
