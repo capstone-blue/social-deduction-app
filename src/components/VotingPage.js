2ff55851bac1df2fc28c8d31c10433eeeccdc01e
Jason Li <iverefiner@gmail.com>
2020-05-14 22:19:25 -0500

    added a checkIfAllVoted button to validate when all players have voted, a lock votes for when all voted, and a finalize vote to move onto the results page

--- src/components/VotingPage.js
+++ src/components/VotingPage.js
@@ -4,6 +4,7 @@ import { useUserId } from '../context/userContext';
 import { useList, useObjectVal } from 'react-firebase-hooks/database'
 import styled from 'styled-components'
 import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
 import ProgressBar from 'react-bootstrap/ProgressBar'
 import Badge from 'react-bootstrap/Badge'
 import ListGroup from 'react-bootstrap/ListGroup'
@@ -20,18 +21,17 @@ const Title = styled.h1`
 
 
 const VotingPage = ({ match }) => {
-  const [gameSessionRef] = useState(db.ref(`/gameSessions/${match.params.id}`));
-  const [players] = useList(gameSessionRef.child('players'))
   const [userId] = useUserId();
   const [gameSessionId] = useState(match.params.id)
+  const [gameSessionRef] = useState(db.ref(`/gameSessions/${gameSessionId}`));
+  const [players] = useList(gameSessionRef.child('players'))
   const [playerRef] = useState(db.ref(`/gameSessions/${gameSessionId}/players/${userId}`))
   const [voteStatusRef] = useState(db.ref(`/gameSessions/${gameSessionId}/players/${userId}/voted`))
   const [playerInfo] = useObjectVal(playerRef)
   const [voted, setVoted] = useState(false)
-
-  function showPlayers() {
-    console.log(players)
-  }
+  const [voteDone, setVoteDone] = useState(false)
+  const [voteLock, setVoteLock] = useState(false)
+  const [isHost, setIsHost] = useState(false)
 
   useEffect(() => {
     function listenOnVoteStatus() {
@@ -47,40 +47,79 @@ const VotingPage = ({ match }) => {
       } catch (e) {
         console.error('Error in VotingPage vote status listener', e.message)
       }
-    } listenOnVoteStatus()
-  }, [userId, gameSessionId, voteStatusRef])
+    }
+    function checkIfHost() {
+      db.ref(`/gameSessions/${gameSessionId}/players/${userId}`).once('value').then((snapshot) => {
+        if (snapshot.val().host === true) {
+          setIsHost(true)
+        }
+      })
+    }
+    checkIfHost()
+    listenOnVoteStatus()
+  }, [userId, gameSessionId, voteStatusRef, playerInfo])
 
   async function vote(selectedPlayer) {
     // if you click on a player, it will set a new vote property onto the game session with that player role
     // or should there be a use effect when entering the vote screen that adds all the players's roles into the game session then increment as voting happens?
     // but we also want to display the votes across all player screens to incite tension
+    if (selectedPlayer.key === userId) {
+      return alert("You can't vote for yourself!")
+    }
     if (!voted) {
       // updated the selected player's vote count +1
       await gameSessionRef.child('players').child(selectedPlayer.key).update({ ...selectedPlayer.val(), votes: selectedPlayer.val().votes + 1 })
       // update the voter's voted status to true
-      await playerRef.update({...playerInfo, voted: true})
-    } else {
-      alert('You already voted!')
+      await playerRef.update({ ...playerInfo, voted: true })
     }
   }
 
   async function unvote(selectedPlayer) {
     // updated the selected player's vote count -1
-    await gameSessionRef.child('players').child(selectedPlayer.key).update({ ...selectedPlayer.val(), votes: selectedPlayer.val().votes - 1 })
-    // update the voter's voted status back to false
-    await playerRef.update({...playerInfo, voted: false })
+    if (voted) {
+      await gameSessionRef.child('players').child(selectedPlayer.key).update({ ...selectedPlayer.val(), votes: selectedPlayer.val().votes - 1 })
+      // update the voter's voted status back to false
+      await playerRef.update({ ...playerInfo, voted: false })
+    }
+  }
+
+  async function checkIfAllVoted() {
+    // go through all players and check vote status
+    // if any returns false, return false
+    const voteResults = players.find(player => player.val().voted === false)
+    if (voteResults) {
+      console.log(false)
+    } else {
+      console.log(true)
+    }
+  }
+
+  function lockVotes() {
+    if (voteLock) {
+      setVoteLock(false)
+    } else {
+      setVoteLock(true)
+    }
+  }
+
+  function finishVoting() {
+    if (checkIfAllVoted() === true) {
+      console.log(playerInfo.host)
+      console.log(players)
+    } else {
+      alert("People aren't done voting!")
+    }
   }
 
   return (
     <React.Fragment>
       <Container>
         <Title><Badge variant="dark">Vote</Badge></Title>
-        <ProgressBar now={60} label='Time Remaining: ' />
+        <ProgressBar now={70} label='Time Remaining: (Not working)' />
         <Container>
           <ListGroup>
-            <ListGroup.Item action onClick={showPlayers}>console.log(players)</ListGroup.Item>
             {players.map(player =>
-              <ListGroup.Item key={player.key} action onClick={!voted ? () => vote(player) : () => unvote(player)}>
+              <ListGroup.Item key={player.key} action onClick={!voted ? () => vote(player) : () => unvote(player)} disabled={voteLock ? true : false}>
                 <Container>
                   <Badge variant="info">{player.val().alias}</Badge>
                   <Badge variant="danger">Votes: {player.val().votes}</Badge>
@@ -88,6 +127,11 @@ const VotingPage = ({ match }) => {
               </ListGroup.Item>
             )}
           </ListGroup>
+        </Container>
+        <Container>
+          {isHost ? <Button variant="success" onClick={() => checkIfAllVoted()}>Check if all voted</Button> : null}
+          {isHost ? <Button variant="warning" onClick={() => lockVotes()}>Lock In Votes</Button> : null}
+          {isHost ? <Button variant="dark" onClick={() => finishVoting()}>Finalize</Button> : null}
         </Container>
       </Container>
     </React.Fragment>
