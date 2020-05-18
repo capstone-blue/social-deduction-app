9ded4e15c0b4be7db5599e980f9d92ca04a30baa
Ian Logan <itlogan9@gmail.com>
2020-05-18 12:38:00 -0500

    Test format evil-button

--- src/components/EvilButton.js
+++ src/components/EvilButton.js
@@ -1,25 +1,36 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
-function EvilButton(props){
-    const [active, setActive] = useState(false)
-    function clickActivate(){
-        setActive(!active)
-        props.buttonClicked(props.role)
-    }
-    return(
-      props.currentRolesList
-
-      ? props.currentRolesList.includes(props.role)
-        ?<Button variant="danger" onClick ={()=>clickActivate()} size = "lg" block >
-              {props.role}
-        </Button>
-        :<Button variant= "outline-danger" onClick ={()=>clickActivate()} size = "lg" block >
-            {props.role}
-          </Button>
-      : <Button variant= "outline-danger" onClick ={()=>clickActivate()} size = "lg" block >
-          {props.role}
-        </Button>
+function EvilButton(props) {
+  const [active, setActive] = useState(false);
+  function clickActivate() {
+    setActive(!active);
+    props.buttonClicked(props.role);
+  }
+  return props.currentRolesList ? (
+    props.currentRolesList.includes(props.role) ? (
+      <Button variant="danger" onClick={() => clickActivate()} size="lg" block>
+        {props.role}
+      </Button>
+    ) : (
+      <Button
+        variant="outline-danger"
+        onClick={() => clickActivate()}
+        size="lg"
+        block
+      >
+        {props.role}
+      </Button>
     )
+  ) : (
+    <Button
+      variant="outline-danger"
+      onClick={() => clickActivate()}
+      size="lg"
+      block
+    >
+      {props.role}
+    </Button>
+  );
 }
 
-export default EvilButton
+export default EvilButton;
