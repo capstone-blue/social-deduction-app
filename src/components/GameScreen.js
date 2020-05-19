63b18b61c18ccff8f383aa1cf35f1db6d3708455
Ian Logan <itlogan9@gmail.com>
2020-05-19 18:11:02 -0500

    Remove navbar from game-screen

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -1,36 +1,31 @@
-import RoleAssignment from './RoleAssignment'
-import WerewolfGamePage from './WerewolfGamePage'
-import DayTime from './DayTime/DayTime'
-import VotingPage from './VotingPage'
-import NavigationBar from './NavigationBar'
-import GameEnd from './GameEnd'
-import React, { useEffect, useState } from 'react';
+import RoleAssignment from './RoleAssignment';
+import WerewolfGamePage from './WerewolfGamePage';
+import DayTime from './DayTime/DayTime';
+import VotingPage from './VotingPage';
+import GameEnd from './GameEnd';
+import React from 'react';
 import { db } from '../firebase';
-import { useObjectVal, useList, useListKeys } from 'react-firebase-hooks/database';
+import { useObjectVal } from 'react-firebase-hooks/database';
 
-function GameScreen({match,history}){
-    const gameRef = db.ref().child('gameSessions').child(match.params.id)
-    const [status] = useObjectVal(gameRef.child("status"))
+function GameScreen({ match, history }) {
+  const gameRef = db.ref().child('gameSessions').child(match.params.id);
+  const [status] = useObjectVal(gameRef.child('status'));
 
-    return(
-
-        <React.Fragment>
-        <NavigationBar/>
-        {status === "roleSelect"
-        ? <RoleAssignment match = {match}/>
-        : status === "nightPhase"
-           ? <WerewolfGamePage match = {match}/>
-           : status === "dayPhase"
-            ? <DayTime match = {match}/>
-            :status === "voting"
-                ? <VotingPage match = {match} history = {history}/>
-                :status ==="results"
-                    ?<GameEnd match = {match} history = {history}/>
-                    :null
-        }
+  return (
+    <React.Fragment>
+      {status === 'roleSelect' ? (
+        <RoleAssignment match={match} />
+      ) : status === 'nightPhase' ? (
+        <WerewolfGamePage match={match} />
+      ) : status === 'dayPhase' ? (
+        <DayTime match={match} />
+      ) : status === 'voting' ? (
+        <VotingPage match={match} history={history} />
+      ) : status === 'results' ? (
+        <GameEnd match={match} history={history} />
+      ) : null}
     </React.Fragment>
-        
-    )
+  );
 }
 
-export default GameScreen
+export default GameScreen;
