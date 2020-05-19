7430539bac0ff79da7fbdde430baf53489e1aa13
Jason Li <iverefiner@gmail.com>
2020-05-19 10:08:07 -0500

    updated node to latest to work with linter

--- src/components/GameEnd.js
+++ src/components/GameEnd.js
@@ -1,14 +1,18 @@
-import React, { useState, useEffect } from 'react'
-import { db } from '../firebase'
+import React, { useState, useEffect } from 'react';
+import { db } from '../firebase';
 import { useUserId } from '../context/userContext';
-import { useList, useObject, useObjectVal } from 'react-firebase-hooks/database'
-import styled from 'styled-components'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button'
-import Badge from 'react-bootstrap/Badge'
-import Form from 'react-bootstrap/Form'
+import {
+  useList,
+  useObject,
+  useObjectVal,
+} from 'react-firebase-hooks/database';
+import styled from 'styled-components';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
+import Form from 'react-bootstrap/Form';
 
 const Title = styled.h1`
   font-size: 2.5em;
@@ -18,67 +22,76 @@ const Title = styled.h1`
 
 const GameEnd = ({ match, history }) => {
   const [userId] = useUserId();
-  const [gameSessionId] = useState(match.params.id)
+  const [gameSessionId] = useState(match.params.id);
   const [gameSessionRef] = useState(db.ref(`/gameSessions/${gameSessionId}`));
-  const [players] = useList(gameSessionRef.child('players'))
-  const [playerRef] = useState(db.ref(`/gameSessions/${gameSessionId}/players/${userId}`))
-  const [isHost, setIsHost] = useState(false)
-  const [winner, setWinner] = useState('')
+  const [players] = useList(gameSessionRef.child('players'));
+  const [playerRef] = useState(
+    db.ref(`/gameSessions/${gameSessionId}/players/${userId}`)
+  );
+  const [isHost, setIsHost] = useState(false);
+  const [winner, setWinner] = useState('');
   const [lobbiesRef] = useState(db.ref().child('lobbies'));
-  const [newLobbyRef] = useState(gameSessionRef.child('newLobby'))
+  const [newLobbyRef] = useState(gameSessionRef.child('newLobby'));
   const [usersRef] = useState(db.ref().child('users'));
   const [lobbyName, setLobbyName] = useState('');
   const [userSnap, userLoading] = useObject(usersRef.child(userId));
 
-
   useEffect(() => {
     function setWinningTeam() {
       try {
-        gameSessionRef.child('winner').once('value').then(function (snapshot) {
-          setWinner(snapshot.val())
-        })
+        gameSessionRef
+          .child('winner')
+          .once('value')
+          .then(function (snapshot) {
+            setWinner(snapshot.val());
+          });
       } catch (e) {
-        console.error('Error in GameEnd setWinningTeam', e.message)
+        console.error('Error in GameEnd setWinningTeam', e.message);
       }
     }
     function checkIfHost() {
-      db.ref(`/gameSessions/${gameSessionId}/players/${userId}/host`).once('value').then((snapshot) => {
-        if (snapshot.exists()) {
-          setIsHost(true)
-        }
-      })
+      db.ref(`/gameSessions/${gameSessionId}/players/${userId}/host`)
+        .once('value')
+        .then((snapshot) => {
+          if (snapshot.exists()) {
+            setIsHost(true);
+          }
+        });
     }
     function listenOnNewLobby() {
       try {
         newLobbyRef.on('value', function (snapshot) {
           if (snapshot.exists() && !isHost) {
-            setLobbyName(snapshot.val())
+            setLobbyName(snapshot.val());
           }
-        })
+        });
       } catch (e) {
-        console.error('Error in GameStart lobby listener', e.message)
+        console.error('Error in GameStart lobby listener', e.message);
       }
     }
-    setWinningTeam()
-    checkIfHost()
-    listenOnNewLobby()
-  }, [gameSessionRef, gameSessionId, userId, isHost])
+    setWinningTeam();
+    checkIfHost();
+    listenOnNewLobby();
+  }, [gameSessionRef, gameSessionId, userId, isHost]);
 
   async function createLobby() {
     try {
       const lobbySnap = await lobbiesRef.push({
         name: lobbyName,
-        status: "pending",
+        status: 'pending',
         players: { [userSnap.key]: { ...userSnap.val(), host: true } },
       });
-      await db.ref().child('users').child(userId).update({ ...userSnap.val, inLobby: true })
-      await gameSessionRef.child('newLobby').set(lobbyName)
+      await db
+        .ref()
+        .child('users')
+        .child(userId)
+        .update({ ...userSnap.val, inLobby: true });
+      await gameSessionRef.child('newLobby').set(lobbyName);
       history.push(`/lobbies/${lobbySnap.key}`);
     } catch (e) {
       console.error('Error in createLobby', e.message);
     }
   }
-
 
   async function joinLobby() {
     try {
@@ -96,8 +109,12 @@ const GameEnd = ({ match, history }) => {
         history.push(`/lobbies/${l.key}`);
         return true;
       });
-      await db.ref().child('users').child(userId).update({ ...userSnap.val, inLobby: true })
-      await newLobbyRef.off()
+      await db
+        .ref()
+        .child('users')
+        .child(userId)
+        .update({ ...userSnap.val, inLobby: true });
+      await newLobbyRef.off();
     } catch (e) {
       console.error('Error in joinLobby', e.message);
     }
@@ -106,26 +123,46 @@ const GameEnd = ({ match, history }) => {
   return (
     <React.Fragment>
       <Container>
-        <Title><Badge variant="dark">Result</Badge></Title>
-        <Title><Badge variant="danger">{winner} {winner === 'Tanner' ? "Wins!" : "Win!"}</Badge></Title>
+        <Title>
+          <Badge variant="dark">Result</Badge>
+        </Title>
+        <Title>
+          <Badge variant="danger">
+            {winner} {winner === 'Tanner' ? 'Wins!' : 'Win!'}
+          </Badge>
+        </Title>
       </Container>
       <Container>
         <Title>Start New Game?</Title>
         <Row>
-          <Form.Control size="sm" type="text" placeholder="Lobby Name" onChange={(e) => setLobbyName(e.target.value)}
-            value={lobbyName} />
+          <Form.Control
+            size="sm"
+            type="text"
+            placeholder="Lobby Name"
+            onChange={(e) => setLobbyName(e.target.value)}
+            value={lobbyName}
+          />
         </Row>
         <Row>
           <Col>
-            {!isHost && lobbyName ? <Button variant="dark" onClick={joinLobby}>Join Lobby</Button> : null}
-            {!isHost && !lobbyName ? <p>Waiting on host to create new lobby...</p> : null}
-            {isHost ? <Button variant="dark" onClick={createLobby}>Create Lobby</Button> : null}
+            {!isHost && lobbyName ? (
+              <Button variant="dark" onClick={joinLobby}>
+                Join Lobby
+              </Button>
+            ) : null}
+            {!isHost && !lobbyName ? (
+              <p>Waiting on host to create new lobby...</p>
+            ) : null}
+            {isHost ? (
+              <Button variant="dark" onClick={createLobby}>
+                Create Lobby
+              </Button>
+            ) : null}
           </Col>
         </Row>
       </Container>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default GameEnd
-
+export default GameEnd;
