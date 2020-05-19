dfbd8b18e84ba7e0ce582d4c078c07c0c7fcf1c8
Ian Logan <itlogan9@gmail.com>
2020-05-19 12:01:21 -0500

    Initial navbar redesign

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,25 +1,34 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import logo from '../assets/logo.jpeg'
+import logo from '../assets/logo.jpeg';
+
+const CustomNavbar = styled(Navbar)`
+  background-color: #2a4365;
+`;
 
 const NavigationBar = () => {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="/">
+    <CustomNavbar>
+      <Navbar.Brand style={{ color: 'white' }} href="/">
         <img
           alt=""
           src={logo}
           width="30"
           height="30"
           className="d-inline-block align-top"
-        /> One Night Ultimate Werewolf
-    </Navbar.Brand>
-      <Nav.Link href="/">Return to Lobby (not working)</Nav.Link>
-      <Nav.Link href="/">Return to Game (not working)</Nav.Link>
-    </Navbar>
-  )
-}
+        />{' '}
+        One Night: Ultimate Werewolf
+      </Navbar.Brand>
+      <Nav.Link style={{ color: '#63B3ED' }} href="/">
+        Return to Lobby (not working)
+      </Nav.Link>
+      <Nav.Link style={{ color: '#63B3ED' }} href="/">
+        Return to Game (not working)
+      </Nav.Link>
+    </CustomNavbar>
+  );
+};
 
-export default NavigationBar
-
+export default NavigationBar;
