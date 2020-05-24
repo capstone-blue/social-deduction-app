209b0e2b7cbaa970abd5046fe31a0f5c2197d613
SJMawn <SJmawn@gmail.com>
2020-05-24 01:07:31 -0500

    changed positioning of start game

--- src/components/RoleSelect/RoleAssignment.js
+++ src/components/RoleSelect/RoleAssignment.js
@@ -54,109 +54,18 @@ function RoleAssignment({ match }) {
       gameRef.update({ currentRoles: newRoles });
     }
   }
-  return playerVals ? (
+  return (
     <Container>
-      <h1 className="text-center">werewolves</h1>
-      <div className="werewolfTeam">
-        <EvilButton
-          buttonClicked={buttonClicked}
-          role="werewolf 1"
-          currentRolesList={currentRolesList}
-        />
-        {` `}
-        <EvilButton
-          buttonClicked={buttonClicked}
-          role="werewolf 2"
-          currentRolesList={currentRolesList}
-        />
-        {` `}
-        <EvilButton
-          buttonClicked={buttonClicked}
-          role="Minion"
-          currentRolesList={currentRolesList}
-        />{' '}
-      </div>
-      <h1 className="text-center">Villagers</h1>
-      <VillageButton
-        buttonClicked={buttonClicked}
-        role="seer"
-        currentRolesList={currentRolesList}
-      />{' '}
-      <VillageButton
-        buttonClicked={buttonClicked}
-        role="robber"
-        currentRolesList={currentRolesList}
-      />{' '}
-      <VillageButton
-        buttonClicked={buttonClicked}
-        role="Troublemaker"
-        currentRolesList={currentRolesList}
-      />{' '}
-      <VillageButton
-        buttonClicked={buttonClicked}
-        role="Drunk"
-        currentRolesList={currentRolesList}
-      />{' '}
-      <VillageButton
-        buttonClicked={buttonClicked}
-        role="Tanner"
-        currentRolesList={currentRolesList}
-      />{' '}
-      <VillageButton
-        buttonClicked={buttonClicked}
-        role="Hunter"
-        currentRolesList={currentRolesList}
-      />{' '}
-      <VillageButton
-        buttonClicked={buttonClicked}
-        role="Insomniac"
-        currentRolesList={currentRolesList}
-      />{' '}
-      <MasonButton masonButtonClicked={masonButtonClicked} />
-      <VillageButton
-        buttonClicked={buttonClicked}
-        role="villager 1"
-        currentRolesList={currentRolesList}
-      />{' '}
-      <VillageButton
-        buttonClicked={buttonClicked}
-        role="villager 2"
-        currentRolesList={currentRolesList}
-      />{' '}
-      <VillageButton
-        buttonClicked={buttonClicked}
-        role="villager 3"
-        currentRolesList={currentRolesList}
-      />{' '}
-      <div>
-        {currentRolesList ? (
-          currentRolesList.length - (players.length + 3) !== 0 ? (
-            <div>
-              {currentRolesList.length - (players.length + 3) > 0 ? (
-                playerVals.host ? (
-                  <div className="text-center">
-                    <h1>Start Game</h1>
-                    <h4 className="text-center">
-                      select {currentRolesList.length - (players.length + 3)}{' '}
-                      fewer roles to start the game{' '}
-                    </h4>
-                  </div>
-                ) : (
-                  <div className="text-center">
-                    <h1>Start Game</h1>
-                    <h4 className="text-center">
-                      host needs to select{' '}
-                      {currentRolesList.length - (players.length + 3)} fewer
-                      roles to start the game{' '}
-                    </h4>
-                  </div>
-                )
-              ) : playerVals.host ? (
+      {currentRolesList ? (
+        currentRolesList.length - (players.length + 3) !== 0 ? (
+          <div>
+            {currentRolesList.length - (players.length + 3) > 0 ? (
+              playerVals.host ? (
                 <div className="text-center">
                   <h1>Start Game</h1>
                   <h4 className="text-center">
-                    select {players.length + 3 - currentRolesList.length} more
-                    roles to start the game
+                    select {currentRolesList.length - (players.length + 3)}{' '}
+                    fewer roles to start the game{' '}
                   </h4>
                 </div>
               ) : (
@@ -164,49 +73,138 @@ function RoleAssignment({ match }) {
                   <h1>Start Game</h1>
                   <h4 className="text-center">
                     host needs to select{' '}
-                    {players.length + 3 - currentRolesList.length} more roles to
-                    start the game
+                    {currentRolesList.length - (players.length + 3)} fewer roles
+                    to start the game{' '}
                   </h4>
                 </div>
-              )}
-            </div>
-          ) : playerVals.host ? (
-            <div className="text-center">
-              <h1>Start Game</h1>
-              <RoleLockButton
-                wolfy={wolfy}
-                roles={currentRolesList}
-                players={players}
-                playersRef={playersRef}
-                roleList={roleList}
-                gameRef={gameRef}
-              />
-            </div>
-          ) : (
-            <div className="text-center">
-              <h1>Start Game</h1>
-              <h4>waiting for host to start the game</h4>
-            </div>
-          )
+              )
+            ) : playerVals.host ? (
+              <div className="text-center">
+                <h1>Start Game</h1>
+                <h4 className="text-center">
+                  select {players.length + 3 - currentRolesList.length} more
+                  roles to start the game
+                </h4>
+              </div>
+            ) : (
+              <div className="text-center">
+                <h1>Start Game</h1>
+                <h4 className="text-center">
+                  host needs to select{' '}
+                  {players.length + 3 - currentRolesList.length} more roles to
+                  start the game
+                </h4>
+              </div>
+            )}
+          </div>
+        ) : playerVals.host ? (
+          <div className="text-center">
+            <h1>Start Game</h1>
+            <RoleLockButton
+              wolfy={wolfy}
+              roles={currentRolesList}
+              players={players}
+              playersRef={playersRef}
+              roleList={roleList}
+              gameRef={gameRef}
+            />
+          </div>
         ) : (
           <div className="text-center">
             <h1>Start Game</h1>
-            <h4>select {players.length + 3} more roles to start the game</h4>
+            <h4>waiting for host to start the game</h4>
           </div>
-        )}
-      </div>
-      <div>
-        <h3 className="text-center">Coming "Soon"</h3>
-        <EvilButton buttonClicked={buttonClicked} role="alpha wolf" />{' '}
-        <VillageButton
-          buttonClicked={buttonClicked}
-          role="Doppelganger"
-          currentRolesList={currentRolesList}
-        />{' '}
-      </div>
+        )
+      ) : (
+        <div className="text-center">
+          <h1>Start Game</h1>
+          <h4>select {players.length + 3} more roles to start the game</h4>
+        </div>
+      )}
+      {playerVals ? (
+        <div>
+          <h1 className="text-center">werewolves</h1>
+          <EvilButton
+            buttonClicked={buttonClicked}
+            role="werewolf 1"
+            currentRolesList={currentRolesList}
+          />
+          {` `}
+          <EvilButton
+            buttonClicked={buttonClicked}
+            role="werewolf 2"
+            currentRolesList={currentRolesList}
+          />
+          {` `}
+          <EvilButton
+            buttonClicked={buttonClicked}
+            role="Minion"
+            currentRolesList={currentRolesList}
+          />{' '}
+          <h1 className="text-center">Villagers</h1>
+          <VillageButton
+            buttonClicked={buttonClicked}
+            role="seer"
+            currentRolesList={currentRolesList}
+          />{' '}
+          <VillageButton
+            buttonClicked={buttonClicked}
+            role="robber"
+            currentRolesList={currentRolesList}
+          />{' '}
+          <VillageButton
+            buttonClicked={buttonClicked}
+            role="Troublemaker"
+            currentRolesList={currentRolesList}
+          />{' '}
+          <VillageButton
+            buttonClicked={buttonClicked}
+            role="Drunk"
+            currentRolesList={currentRolesList}
+          />{' '}
+          <VillageButton
+            buttonClicked={buttonClicked}
+            role="Tanner"
+            currentRolesList={currentRolesList}
+          />{' '}
+          <VillageButton
+            buttonClicked={buttonClicked}
+            role="Hunter"
+            currentRolesList={currentRolesList}
+          />{' '}
+          <VillageButton
+            buttonClicked={buttonClicked}
+            role="Insomniac"
+            currentRolesList={currentRolesList}
+          />{' '}
+          <MasonButton masonButtonClicked={masonButtonClicked} />
+          <VillageButton
+            buttonClicked={buttonClicked}
+            role="villager 1"
+            currentRolesList={currentRolesList}
+          />{' '}
+          <VillageButton
+            buttonClicked={buttonClicked}
+            role="villager 2"
+            currentRolesList={currentRolesList}
+          />{' '}
+          <VillageButton
+            buttonClicked={buttonClicked}
+            role="villager 3"
+            currentRolesList={currentRolesList}
+          />{' '}
+          <h3 className="text-center">Coming "Soon"</h3>
+          <EvilButton buttonClicked={buttonClicked} role="alpha wolf" />{' '}
+          <VillageButton
+            buttonClicked={buttonClicked}
+            role="Doppelganger"
+            currentRolesList={currentRolesList}
+          />{' '}
+        </div>
+      ) : (
+        <h1>loading</h1>
+      )}
     </Container>
-  ) : (
-    <h1>loading</h1>
   );
 }
 
