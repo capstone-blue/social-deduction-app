f50aa224b04ccbc9899890ee8198e3e0ab0b0416
Ian Logan <itlogan9@gmail.com>
2020-05-23 21:04:30 -0500

    Comment voting status out

--- src/components/DayTime/DayCountDown.js
+++ src/components/DayTime/DayCountDown.js
@@ -30,7 +30,7 @@ function DayCountDown({ gameRef, host }) {
         // set an expiration time for 15 seconds into the future
         setEndTimeInDB();
       } else if (countDownReached) {
-        gameRef.child('status').set('voting');
+        // gameRef.child('status').set('voting'); //! Comment this line back in
       }
     }
   }, [gameRef, host, userId, gameHasntStarted, countDownReached]);
