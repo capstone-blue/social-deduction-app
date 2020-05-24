cd3b2b47a6611ee2929cefc9751d6c15be10586b
Ian Logan <itlogan9@gmail.com>
2020-05-24 11:15:14 -0500

    Allow users to only select one card (including their own) during day phase

--- src/components/DayTime/SelectablePlayerCard.js
+++ src/components/DayTime/SelectablePlayerCard.js
@@ -78,11 +78,9 @@ function SelectablePlayerCard({
         isRevealed: false,
         isSelected: true,
       };
-      // if there is a card in the list already, give the new card a different border
-      const firstCard = selectedCards[0];
-      newCard.border =
-        firstCard && firstCard.border === 'green' ? 'red' : 'green';
-      setSelectedCards([...selectedCards, newCard]);
+      // this differs from night phase. we don't need to select 2 cards during the day, so we only account for selecting 1.
+      newCard.border = '#DC3545';
+      setSelectedCards([newCard]);
     }
   }
 
