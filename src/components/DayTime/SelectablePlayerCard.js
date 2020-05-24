53a2825fd1d53ffe07f9e00ff58b07b2189bf706
Ian Logan <itlogan9@gmail.com>
2020-05-24 11:11:16 -0500

    Match day-phase player card styling to night

--- src/components/DayTime/SelectablePlayerCard.js
+++ src/components/DayTime/SelectablePlayerCard.js
@@ -1,15 +1,53 @@
+/* eslint react/no-array-index-key: 0 */
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Badge from 'react-bootstrap/Badge';
-import Card from 'react-bootstrap/Card';
 
-const PlayerCardStyle = styled(Card)`
-  border-width: ${(props) => (props.border ? '3px' : '1px')};
-  border-color: ${(props) => props.border || 'gray'};
+const PlayerContainer = styled.div`
+  position: relative;
+  cursor: pointer;
+  margin: 0 auto;
+  width: 14rem;
+  transition: all 0.1s ease-in-out;
+  bottom: 0;
+  &:hover {
+    bottom: 10rem;
+  }
+`;
+
+const PlayerBadge = styled(Badge)`
+  font-weight: 100;
+  font-size: 1rem;
+`;
+
+const PlayerCard = styled.div`
+  margin-top: 0.5rem;
+  color: #22262a;
+  background-color: #eaeaea;
+  height: auto;
+  width: 14rem;
+  padding: 0.75rem;
+  border: 0.75rem solid transparent;
+  border-left: 0rem solid transparent;
+  border-right: 0rem solid transparent;
+  border-radius: 0.125rem;
+  background-color: #eaeaea;
+  box-shadow: 0 4px 16px 0 rgba(0, 0, 0, 0.1);
+  border-color: ${(props) => props.border || 'transparent'};
+`;
+
+const PlayerCardTitle = styled.div`
+  margin-top: 0.25rem;
+  color: #c22c31;
+  font-size: 1.5rem;
+`;
+
+const PlayerCardContent = styled.div`
+  margin-top: 1rem;
+  font-size: 1rem;
 `;
 
 function SelectablePlayerCard({
-  gameRef,
   setSelectedCards,
   selectedCards,
   cardId,
@@ -49,25 +87,32 @@ function SelectablePlayerCard({
   }
 
   return (
-    <div className="text-center" onClick={handleClick}>
-      <div className="text-center">
-        <Badge pill variant="success" className="text-center">
-          {cardVal.alias}
-        </Badge>
-        <PlayerCardStyle border={card.border}>
-          <Card.Title className="text-center">
-            <div>{cardVal.startingRole.name}</div>
-          </Card.Title>
-          <Card.Body>
-            <div>
-              {cardVal.startingRole.options.map((o, i) => (
-                <div key={`card-${cardId}-${i}`}>{o}</div>
-              ))}
-            </div>
-          </Card.Body>
-        </PlayerCardStyle>
-      </div>
-    </div>
+    <PlayerContainer className="text-center" onClick={handleClick}>
+      <PlayerBadge pill variant="danger" size="lg">
+        {cardVal.alias}
+      </PlayerBadge>
+      <PlayerCard border={card.border}>
+        <PlayerCardTitle className="text-center">
+          <div>{cardVal.startingRole.name}</div>
+        </PlayerCardTitle>
+        <PlayerCardContent>
+          <div>
+            {cardVal.startingRole.options.map((o, i) => (
+              <div key={`card-${cardId}-${i}`}>
+                {i > 0 ? (
+                  <div>
+                    <div style={{ color: '#c22c31', fontSize: '14px' }}>or</div>
+                    <div>{o}</div>
+                  </div>
+                ) : (
+                  o
+                )}
+              </div>
+            ))}
+          </div>
+        </PlayerCardContent>
+      </PlayerCard>
+    </PlayerContainer>
   );
 }
 
