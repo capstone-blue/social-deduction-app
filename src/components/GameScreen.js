f7e42939ecf4c31de4bed784be02ea05bc31abe4
Ian Logan <itlogan9@gmail.com>
2020-05-30 12:46:36 -0500

    Remove video role assignment

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -6,30 +6,10 @@ import GameEnd from './GameEnd';
 import React from 'react';
 import { db } from '../firebase';
 import { useObjectVal } from 'react-firebase-hooks/database';
-import VideoRoleSelect from './RoleSelect/VideoRoleSelect';
 
 function GameScreen({ match, history }) {
   const gameRef = db.ref().child('gameSessions').child(match.params.id);
   const [status] = useObjectVal(gameRef.child('status'));
-  /////////////////////////////////////////
-  // for video use only swap out this code
-
-  // return (
-  //   <React.Fragment>
-  //     {status === 'roleSelect' ? (
-  //       <VideoRoleSelect match={match} />
-  //     ) : status === 'nightPhase' ? (
-  //       <WerewolfGamePage match={match} />
-  //     ) : status === 'dayPhase' ? (
-  //       <DayTime match={match} />
-  //     ) : status === 'voting' ? (
-  //       <VotingPage match={match} history={history} />
-  //     ) : status === 'results' ? (
-  //       <GameEnd match={match} history={history} />
-  //     ) : null}
-  //   </React.Fragment>
-  // );
-  ////////////////////////////////////////////////////////////////
 
   return (
     <React.Fragment>
